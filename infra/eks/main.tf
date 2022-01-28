5f345928506a1d548ed13edbb3a6933e91e80c95
Sandeep Basavaiah <sandeep.basavaiah82@gmail.com>
2022-01-28 16:34:58 -0500

    disabling oidc

--- infra/eks/main.tf
+++ infra/eks/main.tf
@@ -139,10 +139,10 @@ resource "aws_eks_cluster" "main" {
 }
 
 # Fetch OIDC provider thumbprint for root CA
-data "external" "thumbprint" {
-  program =    ["${path.module}/oidc_thumbprint.sh", var.region]
-  depends_on = [aws_eks_cluster.main]
-}
+# data "external" "thumbprint" {
+#   program =    ["${path.module}/oidc_thumbprint.sh", var.region]
+#   depends_on = [aws_eks_cluster.main]
+# }
 
 resource "aws_iam_openid_connect_provider" "main" {
   client_id_list  = ["sts.amazonaws.com"]
