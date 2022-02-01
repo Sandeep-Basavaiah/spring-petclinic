4e8c9005329679e0cd416f223828db85ef6aa939
Sandeep Basavaiah <sandeep.basavaiah82@gmail.com>
2022-02-01 14:03:29 -0500

    Test OIDC Script path

--- infra/eks/main.tf
+++ infra/eks/main.tf
@@ -140,7 +140,7 @@ resource "aws_eks_cluster" "main" {
 
 # Fetch OIDC provider thumbprint for root CA
 data "external" "thumbprint" { 
-  program =    ["${path.module}/oidc_thumbprint.sh", var.region]
+  program =    ["${abspath(path.module)}/oidc_thumbprint.sh", var.region]
   # program =    ["eks/oidc_thumbprint.sh", var.region]
   depends_on = [aws_eks_cluster.main]
 }
